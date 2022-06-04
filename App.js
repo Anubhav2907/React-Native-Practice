f7ca4a54d0b08ad4f0f345aba7a9a1d589293fba
Anxbhxv <as77jadoun@gmail.com>
2022-06-04 14:36:17 +0530

    basic components

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView,Button, StyleSheet,Alert, Text,TouchableOpacity, View, Image, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+    <SafeAreaView style={[styles.container,stylesnew.container]}>
+      <Text style={styles.text} numberOfLines={3}>Open up App.js to start working on your app!!</Text>
+      <TouchableOpacity onPress={()=>console.log('Image')}>
+        <Image source={{
+          width:200,
+          height:200,
+          uri:"https://reactnative.dev/img/tiny_logo.png",
+          }}/>
+      </TouchableOpacity>
+      <View>
+        <Button title='Click Me!' onPress={()=>alert('Button Clicked')}></Button>
+        <Button title='Custom Alert!' onPress={()=>Alert.alert("My Title", "My Message", [
+          {
+            text: "Yes", onPress:()=>{console.log("Yes")}
+          },
+          {
+            text: "No", onPress:()=>{console.log("No")}
+          }
+        ])}></Button>
+        <Button title='Prompt' onPress={()=>Alert.prompt('My Prompt', 'My Message', (text)=>{console.log(text)})}></Button>
+
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'white',
+    paddingBottom:Platform.OS==="ios"?20:0,
+  }
 });
+const stylesnew = StyleSheet.create({
+  container: {
+    color: 'white',
+    backgroundColor: 'teal',
+  }
+})
